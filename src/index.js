6182051cb0f7b1dc81de87091d620fd68c490548
newbie <newbie@newbies-MacBook-Pro.local>
2021-05-13 08:09:48 +0300

    add simple query and mutation

--- src/index.js
+++ src/index.js
@@ -1,19 +1,54 @@
 const { ApolloServer } = require("apollo-server");
+const fs = require("fs");
+const path = require("path");
 
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`;
+const links = [
+  {
+    id: 1,
+    description: "Google",
+    url: "google.com",
+  },
+  {
+    id: 2,
+    description: "Amazon",
+    url: "amazon.com",
+  },
+];
+
+let idCount = links.length;
 
 const resolvers = {
   Query: {
     info: () => "This is API of a Hackernews clone",
+    feed: () => links,
+    link: (parent, args) => {
+      return links.filter((link) => link.id == args.id)[0];
+    },
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: ++idCount,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args) => {
+      links.forEach((link) => {
+        if (link.id == args.id) {
+          link.description = args.description;
+          link.url = args.url;
+        }
+      });
+      return links.filter((link) => link.id == args.id)[0];
+    },
   },
 };
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
 });
 
